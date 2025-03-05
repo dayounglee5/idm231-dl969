11ada8c80be33883814fdcab3c1e05cdfe40dfda
dayounglee5 <dl969@drexel.edu>
2025-03-04 23:46:54 -0500

    Main Code

--- main.js
+++ main.js
@@ -32,116 +32,7 @@ function getZodiac(month, day) {
 // console.log(getZodiac(1, 20));
 
 
-const signs = [
-  {
-    name: 'aries',
-    animal: 'bartholomew bear',
-    image: '/media/pictures/bear,jpg',
-    sound: '/media/sounds/bear-sound-effect.mp3'
-  },
-  {
-    name: 'sagittarius',
-    animal: 'munro scottie dog',
-    image: '/media/pictures/dog.jpg',
-    sound: '/media/sounds/dog-sound-effect.mp3'
-
-  },
-  {
-    name: 'gemini',
-    animal: 'brooke otter',
-    image: '/media/pictures/otter.jpg',    
-    sound: '/media/sounds/otter-sound-effect.mp3'
-
-  },
-  {
-    name: 'aquarius',
-    animal: 'willow hedgehot',
-    image: '/media/pictures/hedgehog.jpg',
-    sound: '/media/sounds/axolotl-sound-effect.mp3'
-
-  },
-  {
-    name: 'cancer',
-    animal: 'delia duck',
-    image: 'duck.jpg',
-    sound: '/media/sounds/duck-sound-effect.mp3'
-
-  },
-  {
-    name: 'leo',
-    animal: 'peanut penguin',
-    image: '/media/pictures/penguin.jpg',
-    sound: '/media/sounds/penguin-sound-effect.mp3'
-
-  },
-  {
-    name: 'libra',
-    animal: 'iris siamese cat',
-    image: '/media/pictures/cat.jpg',
-    sound: '/media/sounds/cat-sound-effect.mp3'
-
-  },
-  {
-    name: 'pisces',
-    animal: 'tully turtle',
-    image: '/media/pictures/turtle.jpg',
-    sound: '/media/sounds/turtle-sound-effect.mp3'
-
-  },
-  {
-    name: 'taurus',
-    animal:'clyde capybara',
-    image: '/media/pictures/capybara.jpg',
-    sound: '/media/sounds/capybara-sound-effect.mp3'
-
-  },
-  {
-    name: 'scorpio',
-    animal: 'odyssey octopus',
-    image: '/media/pictures/octopus.jpg',
-    sound: '/media/sounds/octopus-sound-effect.mp3'
-
-  },
-  {
-    name: 'virgo',
-    animal: 'archie dinosaur',
-    image: '/media/pictures/dinosaur.jpg',
-    sound: '/media/sounds/dinosaur-sound-effect.mp3'
-
-  },
-  {
-    name: 'capricorn',
-    animal: 'myrtle hippopotamus',
-    image: '/media/pictures/hippo.jpg',
-    sound: '/media/sounds/hippo-sound-effect.mp3'
-
-  },
-
-];
-
-// console.log(signs[1]);
-
-
-const closeBtn = document.querySelector('.close-btn');
-function closeCard(){
-  const card = document.getElementById('.jellycat-card');
-  card.style.display = 'none';
-}
-closeBtn.addEventListener('click', closeCard);
-
-
-
-
-const userSubmitButton = document.getElementById('submit-button');
-
-userSubmitButton.addEventListener('click', () => {
-  console.log('button clicked')
-  const userBirthday = document.getElementById('date-picker').value;
-  console.log('this is your birthday ', userBirthday);
-})
-
-
-
+//help 
 const helpImg = document.querySelector('.help-button');
 const helpBubble = document.getElementById('need-help');
 const helpText = document.querySelector('.help-text');
@@ -173,21 +64,311 @@ function collapseHelp() {
 closeButton.addEventListener('click', collapseHelp);
 
 
-const bear = document.getElementById('bear');
-const capybara = document.getElementById('capybara');
-const otter = document.getElementById('otter');
-const duck = document.getElementById('duck');
-const penguin = document.getElementById('penguin');
-const dinosaur = document.getElementById('dinosaur');
-const cat = document.getElementById('cat');
-const octopus = document.getElementById('octopus');
-const dog = document.getElementById('dog');
-const hippo = document.getElementById('hippo');
-const turtle = document.getElementById('turtle'); 
-const hedgehog= document.getElementById('hedgehog');
 
-bear.addEventListener('click', () => {
-  console.log('bear clicked')
-  const audio = document.getElementById('audio-bear');
-  audio.play();
+//bear-card
+document.addEventListener("DOMContentLoaded", function () {
+  const bearImage = document.querySelector("img[alt='bear']");
+  const bearCard = document.getElementById("bear"); 
+  const closeButton = bearCard.querySelector(".close-btn"); 
+  const bearAudio= document.getElementById("audio-bear");
+  
+  function closeCard(){
+    bearCard.classList.add("hidden");
+    bearAudio.pause();
+  }
+  bearImage.addEventListener("click", function () {
+      bearCard.classList.remove("hidden");
+      bearAudio.play();
+
+  });
+
+  closeButton.addEventListener("click", closeCard);
 });
+
+//capybara-card
+document.addEventListener("DOMContentLoaded", function () {
+  const capybaraImage = document.querySelector("img[alt='capybara']");
+  const capybaraCard = document.getElementById("capybara"); 
+  const closeButton = capybaraCard.querySelector(".close-btn"); 
+  const capybaraAudio= document.getElementById("audio-capybara");
+  
+  function closeCard(){
+    capybaraCard.classList.add("hidden");
+    capybaraAudio.pause();
+  }
+  capybaraImage.addEventListener("click", function () {
+      capybaraCard.classList.remove("hidden");
+      capybaraAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+
+//otter-card
+document.addEventListener("DOMContentLoaded", function () {
+  const otterImage = document.querySelector("img[alt='otter']");
+  const otterCard = document.getElementById("otter"); 
+  const closeButton = otterCard.querySelector(".close-btn"); 
+  const otterAudio= document.getElementById("audio-otter");
+  
+  function closeCard(){
+    otterCard.classList.add("hidden");
+    otterAudio.pause();
+  }
+  otterImage.addEventListener("click", function () {
+      otterCard.classList.remove("hidden");
+      otterAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+//duck-card
+document.addEventListener("DOMContentLoaded", function () {
+  const duckImage = document.querySelector("img[alt='duck']");
+  const duckCard = document.getElementById("duck"); 
+  const closeButton = duckCard.querySelector(".close-btn"); 
+  const duckAudio= document.getElementById("audio-duck");
+  
+  function closeCard(){
+    duckCard.classList.add("hidden");
+    duckAudio.pause();
+  }
+  duckImage.addEventListener("click", function () {
+      duckCard.classList.remove("hidden");
+      duckAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+//penguin-card
+document.addEventListener("DOMContentLoaded", function () {
+  const penguinImage = document.querySelector("img[alt='penguin']");
+  const penguinCard = document.getElementById("penguin"); 
+  const closeButton = penguinCard.querySelector(".close-btn"); 
+  const penguinAudio= document.getElementById("audio-penguin");
+  
+  function closeCard(){
+    penguinCard.classList.add("hidden");
+    penguinAudio.pause();
+  }
+  penguinImage.addEventListener("click", function () {
+      penguinCard.classList.remove("hidden");
+      penguinAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+// dinosaur-card
+document.addEventListener("DOMContentLoaded", function () {
+  const dinosaurImage = document.querySelector("img[alt='dinosaur']");
+  const dinosaurCard = document.getElementById("dinosaur"); 
+  const closeButton = dinosaurCard.querySelector(".close-btn"); 
+  const dinosaurAudio= document.getElementById("audio-dinosaur");
+  
+  function closeCard(){
+    dinosaurCard.classList.add("hidden");
+    dinosaurAudio.pause();
+  }
+  dinosaurImage.addEventListener("click", function () {
+      dinosaurCard.classList.remove("hidden");
+      dinosaurAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+//cat-card
+document.addEventListener("DOMContentLoaded", function () {
+  const catImage = document.querySelector("img[alt='cat']");
+  const catCard = document.getElementById("cat"); 
+  const closeButton = catCard.querySelector(".close-btn"); 
+  const catAudio= document.getElementById("audio-cat");
+  
+  function closeCard(){
+    catCard.classList.add("hidden");
+    catAudio.pause();
+  }
+  catImage.addEventListener("click", function () {
+      catCard.classList.remove("hidden");
+      catAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+//octopus-card
+document.addEventListener("DOMContentLoaded", function () {
+  const octopusImage = document.querySelector("img[alt='octopus']");
+  const octopusCard = document.getElementById("octopus"); 
+  const closeButton = octopusCard.querySelector(".close-btn"); 
+  const octopusAudio= document.getElementById("audio-octopus");
+  
+  function closeCard(){
+    octopusCard.classList.add("hidden");
+    octopusAudio.pause();
+  }
+  octopusImage.addEventListener("click", function () {
+      octopusCard.classList.remove("hidden");
+      octopusAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+//dog-card
+document.addEventListener("DOMContentLoaded", function () {
+  const dogImage = document.querySelector("img[alt='dog']");
+  const dogCard = document.getElementById("dog"); 
+  const closeButton = dogCard.querySelector(".close-btn"); 
+  const dogAudio= document.getElementById("audio-dog");
+  
+  function closeCard(){
+    dogCard.classList.add("hidden");
+    dogAudio.pause();
+  }
+  dogImage.addEventListener("click", function () {
+      dogCard.classList.remove("hidden");
+      dogAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+//hippo-card
+document.addEventListener("DOMContentLoaded", function () {
+  const hippoImage = document.querySelector("img[alt='hippo']");
+  const hippoCard = document.getElementById("hippo"); 
+  const closeButton = hippoCard.querySelector(".close-btn"); 
+  const hippoAudio= document.getElementById("audio-hippo");
+  
+  function closeCard(){
+    hippoCard.classList.add("hidden");
+    hippoAudio.pause();
+  }
+  hippoImage.addEventListener("click", function () {
+      hippoCard.classList.remove("hidden");
+      hippoAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+//hedgehog-card
+document.addEventListener("DOMContentLoaded", function () {
+  const hedgehogImage = document.querySelector("img[alt='hedgehog']");
+  const hedgehogCard = document.getElementById("hedgehog"); 
+  const closeButton = hedgehogCard.querySelector(".close-btn"); 
+  const hedgehogAudio= document.getElementById("audio-hedgehog");
+  
+  function closeCard(){
+    hedgehogCard.classList.add("hidden");
+    hedgehogAudio.pause();
+    
+  }
+  hedgehogImage.addEventListener("click", function () {
+      hedgehogCard.classList.remove("hidden");
+      hedgehogAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+
+//turtle-card
+document.addEventListener("DOMContentLoaded", function () {
+  const turtleImage = document.querySelector("img[alt='turtle']");
+  const turtleCard = document.getElementById("turtle"); 
+  const closeButton = turtleCard.querySelector(".close-btn"); 
+  const turtleAudio= document.getElementById("audio-turtle");
+  
+  function closeCard(){
+    turtleCard.classList.add("hidden");
+    turtleAudio.pause();
+    
+  }
+  turtleImage.addEventListener("click", function () {
+      turtleCard.classList.remove("hidden");
+      turtleAudio.play();
+  });
+
+  closeButton.addEventListener("click", closeCard);
+});
+
+// user input
+const userSubmitButton = document.getElementById('submit-button');
+
+userSubmitButton.addEventListener('click', (event) => {
+  event.preventDefault();
+  const dateInput = document.getElementById('date-picker');
+  const userBirthday = new Date(dateInput.value);
+  const month = userBirthday.getMonth()+1;
+  const day = userBirthday.getDate() +1;
+  const year = userBirthday.getFullYear();
+
+  const zodiac = getZodiac(month,day,year);
+  // console.log('your zodiac is', zodiac);
+
+  showCardBasedonZodiac(zodiac);
+
+  // console.log('button clicked')
+  // console.log('this is your birthday ', userBirthday);
+})
+
+function showCardBasedonZodiac(zodiac) {
+
+  const bearCard = document.getElementById('bear');
+  const dogCard = document.getElementById('dog');
+  const otterCard = document.getElementById('otter');
+  const hedgehogCard = document.getElementById('hedgehog');
+  const duckCard = document.getElementById('duck');
+  const penguinCard = document.getElementById('penguin');
+  const catCard = document.getElementById('cat');
+  const turtleCard = document.getElementById('turtle');
+  const capybaraCard = document.getElementById('capybara');
+  const octopusCard = document.getElementById('octopus');
+  const dinosaurCard = document.getElementById('dinosaur');
+  const hippoCard = document.getElementById('hippo');
+
+  if (zodiac === 'aries') {
+    bearCard.classList.remove('hidden');
+    document.getElementById('audio-bear').play();
+  } else if (zodiac === 'sagittarius') {
+    dogCard.classList.remove('hidden');
+    document.getElementById('audio-dog').play();
+  } else if (zodiac === 'gemini') {
+    otterCard.classList.remove('hidden');
+    document.getElementById('audio-otter').play();
+  } else if (zodiac === 'aquarius') {
+    hedgehogCard.classList.remove('hidden');
+    document.getElementById('audio-hedgehog').play();
+  } else if (zodiac === 'cancer') {
+    duckCard.classList.remove('hidden');
+    document.getElementById('audio-duck').play();
+  } else if (zodiac === 'leo') {
+    penguinCard.classList.remove('hidden');
+    document.getElementById('audio-penguin').play();
+  } else if (zodiac === 'libra') {
+    catCard.classList.remove('hidden');
+    document.getElementById('audio-cat').play();
+  } else if (zodiac === 'pisces') {
+    turtleCard.classList.remove('hidden');
+    document.getElementById('audio-turtle').play();
+  } else if (zodiac === 'taurus') {
+    capybaraCard.classList.remove('hidden');
+    document.getElementById('audio-capybara').play();
+  } else if (zodiac === 'scorpio') {
+    octopusCard.classList.remove('hidden');
+    document.getElementById('audio-octopus').play();
+  } else if (zodiac === 'virgo') {
+    dinosaurCard.classList.remove('hidden');
+    document.getElementById('audio-dinosaur').play();
+  } else if (zodiac === 'capricorn') {
+    hippoCard.classList.remove('hidden');
+    document.getElementById('audio-hippo').play();
+  }
+
+}
